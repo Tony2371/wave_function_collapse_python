54b87b5e4892482fbcca17e4caab08bb44618dee
Anton <Anton@venomex.com>
2023-11-13 10:26:24 +0300

    - cursor object now tracks tile's rotation

--- utils.py
+++ utils.py
@@ -9,11 +9,11 @@ class TileMap:
         self.tiles = [Structure(x*scale_multiplier, y*scale_multiplier) for x in range(size_x) for y in range(size_y)]
 
 class Structure:
-    def __init__(self, pos_x, pos_y, tile_type='floor_outside'):
+    def __init__(self, pos_x, pos_y, tile_name='floor_outside'):
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.type = type
-        self.loaded_texture = pygame.image.load(f'textures/{tile_type}.png')
+        self.name = tile_name
+        self.loaded_texture = pygame.image.load(f'textures/{tile_name}.png')
 
 class Cursor:
     def __init__(self):
@@ -21,6 +21,8 @@ class Cursor:
         self.pos_y = 0
         self.available_tiles = [tile_name.replace('.png', '') for tile_name in os.listdir('textures')]
         self.selected_tile_index = 0
+        self.selected_tile_name = self.available_tiles[self.selected_tile_index]
+        self.selected_rotation = 0
         self.loaded_texture = pygame.image.load(f'textures/{self.available_tiles[self.selected_tile_index]}.png')
         self.loaded_texture.set_alpha(128)
 
@@ -31,4 +33,13 @@ class Cursor:
             
         self.loaded_texture = pygame.image.load(f'textures/{self.available_tiles[self.selected_tile_index]}.png')
         self.loaded_texture.set_alpha(128)
+        self.selected_tile_name = self.available_tiles[self.selected_tile_index]
+        self.selected_rotation = 0
+    
+    def rotate_tile(self):
+        self.loaded_texture = pygame.transform.rotate(self.loaded_texture, -90)
+        self.selected_rotation -= 90
+        if self.selected_rotation <= -360:
+            self.selected_rotation = 0
         
+        print(self.selected_rotation)
