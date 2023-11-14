9b21af68e96e9c40deaaf5987ada187e148094a8
Anton <Anton@venomex.com>
2023-11-14 12:12:15 +0300

    - crated samples are now saved into json file

--- generate_sample.py
+++ generate_sample.py
@@ -2,6 +2,8 @@ import json
 from utils import *
 import pygame
 from math import floor
+import pickle
+import time
 
 with open('configuration.json', 'r') as config_file:
     config = json.load(config_file)
@@ -26,25 +28,55 @@ while running:
             running = False
         
         elif event.type == pygame.KEYDOWN:  # Check if the event is KEYDOWN
-            if event.key == pygame.K_r:
-                cursor.switch_tile()
+            if event.key == pygame.K_e:
+                cursor.rotate_tile()
+            
+            if event.key == pygame.K_s:
+               tilemap.generate_rules()
+            
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for tile in tilemap.tiles:
+                if tile.pos_x == cursor.pos_x and tile.pos_y == cursor.pos_y:
+                    if event.button == 1:
+                        tile.update_tile(cursor.selected_tile_name, cursor.selected_rotation)
+                    if event.button == 3:
+                        tile.update_tile('floor_outside', 0)
+                    
+                    if event.button == 4:
+                        cursor.switch_tile_foward()
+                    if event.button == 5:
+                        cursor.switch_tile_backwards()
                 
 
     mouse_x, mouse_y = pygame.mouse.get_pos()
     cursor.pos_x = floor(mouse_x/100)*100
     cursor.pos_y = floor(mouse_y/100)*100
 
-
-
     # GENERAL RENDERING PART
     for tile in tilemap.tiles:
         screen.blit(tile.loaded_texture, (tile.pos_x, tile.pos_y))
+
+    '''
+    for tile in set(tilemap.tiles):
+        if tile.name != 'floor_outside':
+            font = pygame.font.Font('freesansbold.ttf', 16)
+            text = font.render(f'{tile.name}', True, (0, 0, 0))
+            all_x = [i.pos_x for i in tilemap.tiles if i.name == tile.name]
+            sum_pos_x = sum(all_x)/len(all_x)
+            all_y = [i.pos_y for i in tilemap.tiles if i.name == tile.name]
+            sum_pos_y = sum(all_y)/len(all_y)
+
+            screen.blit(text, (sum_pos_x, sum_pos_y+45))
+    '''
     
     screen.blit(cursor.loaded_texture, (cursor.pos_x, cursor.pos_y)) # cursor should be rendered above everything
-
-
+    font = pygame.font.Font('freesansbold.ttf', 16)
+    text = font.render(f'Selected tile: {cursor.selected_tile_name}', True, (0, 0, 0))
+    screen.blit(text, (10, 10))
+    
     pygame.display.flip()
     clock.tick(30)
 
 
 pygame.quit()
+
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import time
+import json
 
 class TileMap:
     def __init__(self, size_x, size_y, scale_multiplier=100):
@@ -8,30 +10,85 @@ class TileMap:
         self.scale_multiplier = scale_multiplier
         self.tiles = [Structure(x*scale_multiplier, y*scale_multiplier) for x in range(size_x) for y in range(size_y)]
 
+    def generate_rules(self):
+        rules_dict = {}
+        for tile in self.tiles:
+            if f'{tile.name}|{tile.rotation}' not in rules_dict:
+                rules_dict[f'{tile.name}|{tile.rotation}'] = {'up': set(),
+                                                              'down': set(),
+                                                              'left': set(),
+                                                              'right': set()}
+            try:                
+                rules_dict[f'{tile.name}|{tile.rotation}']['up'].add([f'{i.name}|{i.rotation}' for i in self.tiles if i.pos_x == tile.pos_x and i.pos_y == tile.pos_y-100][0])
+            except:
+                pass
+            try:
+                rules_dict[f'{tile.name}|{tile.rotation}']['down'].add([f'{i.name}|{i.rotation}' for i in self.tiles if i.pos_x == tile.pos_x and i.pos_y == tile.pos_y+100][0])
+            except:
+                pass
+            try:
+                rules_dict[f'{tile.name}|{tile.rotation}']['left'].add([f'{i.name}|{i.rotation}' for i in self.tiles if i.pos_x == tile.pos_x-100 and i.pos_y == tile.pos_y][0])
+            except:
+                pass
+            try:
+                rules_dict[f'{tile.name}|{tile.rotation}']['right'].add([f'{i.name}|{i.rotation}' for i in self.tiles if i.pos_x == tile.pos_x+100 and i.pos_y == tile.pos_y][0])
+            except:
+                pass
+
+        for value in rules_dict.keys():
+            print(value)
+            print(rules_dict[value])
+        
+
+        for key in rules_dict:
+            for direction in rules_dict[key]:
+                rules_dict[key][direction] = list(rules_dict[key][direction])
+        with open(f'generated_samples/rules_{time.time()}.json', 'w') as file:
+            json.dump(rules_dict, file)
+        print('--------------------------')
+
 class Structure:
-    def __init__(self, pos_x, pos_y, tile_name='floor_outside'):
+    def __init__(self, pos_x, pos_y, tile_name='floor_outside', rotation=0):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.name = tile_name
-        self.loaded_texture = pygame.image.load(f'textures/{tile_name}.png')
+        self.rotation = rotation
+        self.loaded_texture = pygame.image.load(f'textures_structures/{tile_name}.png')
+        self.loaded_texture = pygame.transform.rotate(self.loaded_texture, self.rotation)
+
+    def update_tile(self, tile_name, rotation):
+        self.name = tile_name
+        self.rotation = rotation
+        self.loaded_texture = pygame.image.load(f'textures_structures/{tile_name}.png')
+        self.loaded_texture = pygame.transform.rotate(self.loaded_texture, self.rotation)
 
 class Cursor:
     def __init__(self):
         self.pos_x = 0
         self.pos_y = 0
-        self.available_tiles = [tile_name.replace('.png', '') for tile_name in os.listdir('textures')]
+        self.available_tiles = [tile_name.replace('.png', '') for tile_name in os.listdir('textures_structures')]
         self.selected_tile_index = 0
         self.selected_tile_name = self.available_tiles[self.selected_tile_index]
         self.selected_rotation = 0
-        self.loaded_texture = pygame.image.load(f'textures/{self.available_tiles[self.selected_tile_index]}.png')
+        self.loaded_texture = pygame.image.load(f'textures_structures/{self.available_tiles[self.selected_tile_index]}.png')
         self.loaded_texture.set_alpha(128)
 
-    def switch_tile(self):
+    def switch_tile_foward(self):
         self.selected_tile_index += 1
         if self.selected_tile_index >= len(self.available_tiles):
             self.selected_tile_index = 0
             
-        self.loaded_texture = pygame.image.load(f'textures/{self.available_tiles[self.selected_tile_index]}.png')
+        self.loaded_texture = pygame.image.load(f'textures_structures/{self.available_tiles[self.selected_tile_index]}.png')
+        self.loaded_texture.set_alpha(128)
+        self.selected_tile_name = self.available_tiles[self.selected_tile_index]
+        self.selected_rotation = 0
+    
+    def switch_tile_backwards(self):
+        self.selected_tile_index -= 1
+        if self.selected_tile_index <= -1*len(self.available_tiles)-1:
+            self.selected_tile_index = 0
+        
+        self.loaded_texture = pygame.image.load(f'textures_structures/{self.available_tiles[self.selected_tile_index]}.png')
         self.loaded_texture.set_alpha(128)
         self.selected_tile_name = self.available_tiles[self.selected_tile_index]
         self.selected_rotation = 0
@@ -41,5 +98,4 @@ class Cursor:
         self.selected_rotation -= 90
         if self.selected_rotation <= -360:
             self.selected_rotation = 0
-        
-        print(self.selected_rotation)
+            
